dbc2e120b21fcda2a0fa5ba1dc8b24ca2d547643
rgiglio <rgiglio@gmail.com>
2020-03-17 23:37:02 -0300

    Revert "suggestion for param passing"
    This reverts commit f0ce2f4e06d29dbce29734caad441207a86a8fef.

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-import sys
-sys.path.insert(0, "models")
 import streamlit as st
-from models.seir_bayes import (
-    run_SEIR_BAYES_model, seir_bayes_plot, Model
-    )
+from models.seir_bayes import run_SEIR_BAYES_model, make_normal_scale, seir_bayes_plot
 import matplotlib.pyplot as plt
 
 
-def _run_SEIR_BAYES_model(model):
-    model = run_SEIR_BAYES_model(model)
-    fig = seir_bayes_plot(model)
+
+def _run_SEIR_BAYES_model(N, E0, I0, R0, 
+                          R0__loc, R0__scale,
+                          gamma_loc, gamma_scale,
+                          alpha_loc, alpha_scale,
+                          t_max, runs):
+    S, E, I, R, t_space = run_SEIR_BAYES_model(
+                                        N, E0, I0, R0, 
+                                        R0__loc, R0__scale,
+                                        gamma_loc, gamma_scale,
+                                        alpha_loc, alpha_scale,
+                                        t_max, runs)
+    fig = seir_bayes_plot(N, E0, I0, R0, 
+                          R0__loc, R0__scale,
+                          gamma_loc, gamma_scale,
+                          alpha_loc, alpha_scale,
+                          t_max, runs, S, E, I, R, t_space)
     return fig
 
 
@@ -23,19 +33,14 @@ alpha_loc = float(st.sidebar.text_input('alpha_loc', '0.1923'))
 t_max = int(st.sidebar.text_input('t_max', '180'))
 runs = int(st.sidebar.text_input('runs', '100'))
 
-model = Model(
-    {
-        'N': N,
-        'E0': E0,
-        'I0': I0,
-        'R0': R0,
-        'R0__loc': R0__loc,
-        'gamma_loc': gamma_loc,
-        'alpha_loc': alpha_loc,
-        't_max': t_max,
-        'runs': runs
-    }
-)
+S0 = N - (E0 + I0 + R0)
+R0__scale = make_normal_scale(1.96, 2.55, .95, R0__loc)
+gamma_scale = make_normal_scale(1/14, 1/7, .95, gamma_loc)
+alpha_scale = make_normal_scale(1/7, 1/4.1, .95, alpha_loc)
 
-fig = _run_SEIR_BAYES_model(model)
+fig = _run_SEIR_BAYES_model(N, E0, I0, R0,
+                          R0__loc, R0__scale,
+                          gamma_loc, gamma_scale,
+                          alpha_loc, alpha_scale,
+                          t_max, runs)
 st.pyplot()
--- models/seir_bayes.py
+++ models/seir_bayes.py
@@ -1,32 +1,56 @@
 import numpy as np
 import numpy.random as npr
 from scipy.integrate import odeint
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-from model import Model
+
+def make_normal_scale(lb, ub, ci, loc):
+    z = norm.ppf((1+ci)/2)
+    scale_ub = -(loc - ub)/z
+    return scale_ub
 
 
-def run_SEIR_BAYES_model(m):
-    S = np.zeros(m.size)
-    E = np.zeros(m.size)
-    I = np.zeros(m.size)
-    R = np.zeros(m.size)
-    for r in range(m.runs):
-        S[0, r] = m.S0
-        E[0, r] = m.E0
-        I[0, r] = m.I0
-        R[0, r] = m.R0
+def run_SEIR_BAYES_model(
+        N: 'population size',
+        E0: 'init. exposed population',
+        I0: 'init. infected population',
+        R0: 'init. removed population',
+        R0__loc: 'repr. rate shape',
+        R0__scale: 'repr. rate scale',
+        gamma_loc: 'removal rate shape',
+        gamma_scale: 'removal rate scale',
+        alpha_loc: 'incubation rate shape',
+        alpha_scale: 'incubation rate scale',
+        t_max: 'numer of days to run',
+        runs: 'number of runs'
+    ):
 
-        R0_ = npr.normal(m.R0__loc, m.R0__scale)
-        gamma = npr.normal(m.gamma_loc, m.gamma_scale)
-        alpha = npr.normal(m.alpha_loc, m.alpha_scale)
+    S0 = N - (I0 + R0 + E0)
+    t_space = np.arange(0, t_max)
+
+    size = (t_max, runs)
+
+    S = np.zeros(size)
+    E = np.zeros(size)
+    I = np.zeros(size)
+    R = np.zeros(size)
+    for r in range(runs):
+        S[0, r] = S0
+        E[0, r] = E0
+        I[0, r] = I0
+        R[0, r] = R0
+
+        R0_ = npr.normal(R0__loc, R0__scale)
+        gamma = npr.normal(gamma_loc, gamma_scale)
+        alpha = npr.normal(alpha_loc, alpha_scale)
         beta = R0_*gamma
-        for t in m.t_space[1:]:
-            SE = npr.binomial(S[t-1, r], 1 - np.exp(-beta*I[t-1, r]/m.N))
+        for t in t_space[1:]:
+            SE = npr.binomial(S[t-1, r], 1 - np.exp(-beta*I[t-1, r]/N))
             EI = npr.binomial(E[t-1, r], 1 - np.exp(-alpha))
             IR = npr.binomial(I[t-1, r], 1 - np.exp(-gamma))
 
-            dS = 0 - SE
+            dS =  0 - SE
             dE = SE - EI
             dI = EI - IR
             dR = IR - 0
@@ -35,49 +59,44 @@ def run_SEIR_BAYES_model(m):
             E[t, r] = E[t-1, r] + dE
             I[t, r] = I[t-1, r] + dI
             R[t, r] = R[t-1, r] + dR
-    m.S = S
-    m.E = E
-    m.I = I
-    m.R = R
-    return m
+
+    return S, E, I, R, t_space
 
 
-def seir_bayes_plot(m):
+def seir_bayes_plot(N, E0, I0, R0,
+                    R0__loc, R0__scale,
+                    gamma_loc, gamma_scale,
+                    alpha_loc, alpha_scale,
+                    t_max, runs, S, E, I, R, t_space):
+    S0 = N - (I0 + R0 + E0)
+    # plot
     algorithm_text = (
-        f"for {m.runs} runs, do:\n"
-        f"\t$S_0={m.S0}$\n\t$E_0={m.E0}$\n\t$I_0={m.I0}$\n\t$R_0={m.R0}$\n"
-        f"\t$\\gamma \\sim Normal(\mu={m.gamma_loc:.04},"
-        f"\\sigma={m.gamma_scale:.04})$\n"
-        f"\t$\\alpha \\sim Normal(\mu={m.alpha_loc:.04},"
-        f"\\sigma={m.alpha_scale:.04})$\n"
-        f"\t$R0 \\sim Normal(\mu={m.R0__loc:.04},"
-        f"\\sigma={m.R0__scale:.04})$\n"
+        f"for {runs} runs, do:\n"
+        f"\t$S_0={S0}$\n\t$E_0={E0}$\n\t$I_0={I0}$\n\t$R_0={R0}$\n"
+        f"\t$\\gamma \\sim Normal(\mu={gamma_loc:.04}, \\sigma={gamma_scale:.04})$\n"
+        f"\t$\\alpha \\sim Normal(\mu={alpha_loc:.04}, \\sigma={alpha_scale:.04})$\n"
+        f"\t$R0 \\sim Normal(\mu={R0__loc:.04}, \\sigma={R0__scale:.04})$\n"
         f"\t$\\beta = \\gamma R0$\n"
         f"\tSolve SEIR$(\\alpha, \\gamma, \\beta)$"
     )
 
-    title = ('(RESULTADO PRELIMINAR) Pessoas afetadas '
-             'pelo COVID-19, segundo o modelo SEIR-Bayes')
+    title = '(RESULTADO PRELIMINAR) Pessoas afetadas pelo COVID-19, segundo o modelo SEIR-Bayes'
     plt.style.use('ggplot')
-    fig, ax = plt.subplots(figsize=(16, 9))
-    plt.plot(
-        m.t_space, m.E.mean(axis=1), '--',
-        m.t_space, m.I.mean(axis=1), '--',
-        marker='o'
-        )
+    fig, ax = plt.subplots(figsize=(16,9))
+    plt.plot(t_space, E.mean(axis=1), '--', t_space, I.mean(axis=1), '--', marker='o')
     plt.title(title, fontsize=20)
     plt.legend(['Expostas ($\mu \pm \sigma$)',
                 'Infectadas ($\mu \pm \sigma$)'],
                fontsize=20, loc='lower right')
     plt.xlabel('t (Dias a partir de 16/Março/2020)', fontsize=20)
     plt.ylabel('Pessoas', fontsize=20)
-    plt.fill_between(m.t_space,
-                     m.I.mean(axis=1) + m.I.std(axis=1),
-                     (m.I.mean(axis=1) - m.I.std(axis=1)).clip(m.I0),
+    plt.fill_between(t_space,
+                     I.mean(axis=1) + I.std(axis=1), 
+                     (I.mean(axis=1) - I.std(axis=1)).clip(I0),
                      color='b', alpha=0.2)
-    plt.fill_between(m.t_space,
-                     m.E.mean(axis=1) + m.E.std(axis=1),
-                     (m.E.mean(axis=1) - m.E.std(axis=1)).clip(m.I0),
+    plt.fill_between(t_space, 
+                     E.mean(axis=1) + E.std(axis=1), 
+                     (E.mean(axis=1) - E.std(axis=1)).clip(I0),
                      color='r', alpha=0.2)
     plt.xticks(fontsize=20)
     plt.yticks(fontsize=20)
@@ -90,7 +109,26 @@ def seir_bayes_plot(m):
 
 
 if __name__ == '__main__':
-    model = Model()
-    model = run_SEIR_BAYES_model(model)
-    fig = seir_bayes_plot(model)
+    N = 13_000_000
+    E0, I0, R0 = 50, 152, 1
+    R0__loc = 2.2
+    R0__scale = make_normal_scale(1.96, 2.55, .95, R0__loc)
+    gamma_loc = 1/10
+    gamma_scale = make_normal_scale(1/14, 1/7, .95, gamma_loc)
+    alpha_loc = 1/5.2
+    alpha_scale = make_normal_scale(1/7, 1/4.1, .95, alpha_loc)
+    t_max = 30*6
+    runs = 100
+    S, E, I, R, t_space = run_SEIR_BAYES_model(
+                                      N, E0, I0, R0,
+                                      R0__loc, R0__scale,
+                                      gamma_loc, gamma_scale,
+                                      alpha_loc, alpha_scale,
+                                      t_max, runs)
+
+    fig = seir_bayes_plot(N, E0, I0, R0,
+                          R0__loc, R0__scale,
+                          gamma_loc, gamma_scale,
+                          alpha_loc, alpha_scale,
+                          t_max, runs, S, E, I, R, t_space)
     plt.show()
