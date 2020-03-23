9b8ca215c278c3c9931c2050a364bcdbe605204d
dsevero <danielsouzasevero@gmail.com>
2020-03-22 21:52:03 -0300

    Update data

--- data/data_params.py
+++ data/data_params.py
@@ -140,37 +140,52 @@ def estimate_R0(value: 'query uf/city value',
 
 def estimate_E0(value: 'query uf/city value',
                 date: 'query uf date',
-                granularity):
+                granularity,
+                method='avg_history'):
     '''
     Premises: Exposed(t)>=New_cases(t-avg_incubation_time)
     uses the last valid E for dates which value is null
     '''
-    avg_incubation_time = 5
-    
-    if granularity == 'Estado':
-        E0 = (load_uf_covid_data()
-              .query('uf == @value')
-              .assign(exposed=lambda df: df.cases
-                                           .shift(-avg_incubation_time)
-                                           .fillna(method='ffill')
-                                           .fillna(0))  
-              .query('date == @date')
-              [['uf', 'exposed']]
-              .values
-              [0][1]
-             )
-    elif granularity == 'Município':
+    if method == 'avg_history':
+        avg_incubation_time = 5
+        
+        if granularity == 'Estado':
+            E0 = (load_uf_covid_data()
+                  .query('uf == @value')
+                  .assign(exposed=lambda df: df.cases
+                                               .shift(-avg_incubation_time)
+                                               .fillna(method='ffill')
+                                               .fillna(0))  
+                  .query('date == @date')
+                  [['uf', 'exposed']]
+                  .values
+                  [0][1]
+                 )
+        elif granularity == 'Município':
+            E0 = (load_city_covid_data()
+                  .query('city == @value')
+                  .assign(exposed=lambda df: df.cases
+                                               .shift(-avg_incubation_time)
+                                               .fillna(method='ffill')
+                                               .fillna(0))  
+                  .query('date == @date')
+                  [['uf', 'exposed']]
+                  .values
+                  [0][1]
+                 )
+    elif method=='double':
         E0 = (load_city_covid_data()
               .query('city == @value')
-              .assign(exposed=lambda df: df.new_cases
-                                           .shift(-avg_incubation_time)
-                                           .fillna(method='ffill')
-                                           .fillna(0))  
+              .assign(exposed=lambda df: 2*df.cases)
               .query('date == @date')
               [['uf', 'exposed']]
               .values
-              [0][1]
-             )
+              [0][1])
+    elif method == 'gradient_transient':
+        g = 1/10
+        a = 1/5.2
+        b = g*3
+        E0_func = lambda S, N: (g - a + np.sqrt((g - a)**2 + 4*a*b*S/N)/(2*a))
     else: 
         E0 = 152
     return int(E0)      
@@ -178,14 +193,15 @@ def estimate_E0(value: 'query uf/city value',
 
 def query_params(value: 'query uf/city value',
                  date: 'query uf date',
-                 granularity):
+                 granularity,
+                 E0_method='avg history'):
     '''
     Query N, I(0), E(0) and R(0) parameters based on historic data
     for a given uf and date
 
     '''
     N = query_N(value, granularity)
-    E0 = estimate_E0(value, date, granularity) # temporary workaround
+    E0 = estimate_E0(value, date, granularity, E0_method) # temporary workaround
     I0 = query_I0(value, date, granularity)
     R0 = estimate_R0(value, date, granularity)
     return N, E0, I0, R0
