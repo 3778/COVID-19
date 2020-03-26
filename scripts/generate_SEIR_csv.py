7111aaf6a6c53f7db912c33e87b4fd24899cf304
dsevero <danielsouzasevero@gmail.com>
2020-03-26 16:07:55 -0300

    Add scripts

--- scripts/generate_SEIR_csv.py
+++ scripts/generate_SEIR_csv.py
@@ -13,7 +13,7 @@ model = SEIRBayes.init_from_intervals(NEIR0=(12252023, E0, I0, R0),
                                       r0_interval=(1.9, 5, 0.95),
                                       gamma_inv_interval=(10, 14, 0.95),
                                       alpha_inv_interval=(4.2, 5, 0.95),
-                                      t_max=30)
+                                      t_max=180)
 S, E, I, R, t = model.sample(1000)
 pred = pd.DataFrame(index=(pd.date_range(start=date_for_pred, periods=t.shape[0])
                              .strftime('%Y-%m-%d')),
@@ -25,6 +25,7 @@ pred = pd.DataFrame(index=(pd.date_range(start=date_for_pred, periods=t.shape[0]
 df = (pred
       .join(cases, how='outer')
       .assign(cases=lambda df: df.totalCases.fillna(df.I))
-      .assign(newly_infected=lambda df: df.cases - df.cases.shift(1) + df.R)
+      .assign(newly_infected=lambda df: df.cases - df.cases.shift(1) + df.R - df.R.shift(1))
+      .assign(newly_R=lambda df: df.R.diff())
       .rename(columns={'cases': 'totalCases OR I'}))
-df.to_csv('SEIR_csv.py')
+df.to_csv('seir_output.csv')
